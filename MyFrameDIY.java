8d12cd610dd0e8c5b6ba68c284c59f7a9d7b92b1
MustafaGitZa <sibusisomst@gmail.com>
2024-07-31 00:16:42 +0200

    Add files via upload

---
+++ MyFrameDIY.java
@@ -0,0 +1,19 @@
+
+package ac.za.ui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+
+public class MyFrameDIY extends JFrame{
+
+    public MyFrameDIY() {
+        this.setTitle("Mustafa Xaba");
+        this.setSize(800,850);
+        this.setResizable(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setForeground(Color.gray);
+        this.setVisible(true);
+    }
+    
+}
---
+++ MyFrameDIYApp.java
@@ -0,0 +1,15 @@
+
+package myframediyapp;
+
+import ac.za.ui.MyFrameDIY;
+
+
+public class MyFrameDIYApp {
+
+    
+    public static void main(String[] args) {
+        MyFrameDIY frame = new MyFrameDIY();
+        
+    }
+    
+}
